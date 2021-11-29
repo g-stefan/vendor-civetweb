1c2ef6ec08f855563450eb4db4d5438687dcb14c
Grigore Stefan <g_stefan@yahoo.com>
2021-11-29 16:43:14 +0200

    Update

--- build/msvc.sign.cmd
+++ build/msvc.sign.cmd
@@ -6,6 +6,5 @@ rem Created by Grigore Stefan <g_stefan@yahoo.com>
 echo - %BUILD_PROJECT% ^> sign
 
 pushd output
-for /r %%i in (*.dll) do call grigore-stefan.sign "%BUILD_PROJECT%" "%%i"
-for /r %%i in (*.exe) do call grigore-stefan.sign "%BUILD_PROJECT%" "%%i"
+for /r %%i in (*civetweb.exe) do call grigore-stefan.sign "%BUILD_PROJECT%" "%%i"
 popd
