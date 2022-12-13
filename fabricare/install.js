620d860ebad595c2d0d695baca2cb0648a521181
Grigore Stefan <g_stefan@yahoo.com>
2022-12-13 13:23:27 +0200

    Update

--- fabricare/install.js
+++ fabricare/install.js
@@ -3,6 +3,8 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
+messageAction("install");
+
 Shell.copyFilesToDirectory("output/bin/*", pathRepository + "/bin");
 Shell.copyDirRecursively("output/include", pathRepository + "/include");
 Shell.copyFile("output/lib/civetweb.lib", pathRepository + "/lib/civetweb.lib");
