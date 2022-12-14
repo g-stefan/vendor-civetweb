5cec28b8f95921ebfd009095a3c876b9033d94fb
Grigore Stefan <g_stefan@yahoo.com>
2022-12-14 02:07:21 +0200

    Update

--- fabricare/make.js
+++ fabricare/make.js
@@ -3,7 +3,7 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
-Fabricare.include("project.vendor");
+Fabricare.include("vendor");
 
 messageAction("make");
 
